cccf1812d99d8eba5854c38c94d462787db0aa4c
ali-ahadi1105 <aliahadi.f1105@gmail.com>
2025-12-09 19:57:15 +0330

    add env validator file

---
+++ packages/common/src/env.ts
@@ -0,0 +1,28 @@
+import { ZodObject, ZodRawShape } from 'zod';
+
+interface EnvOptions {
+    source: NodeJS.ProcessEnv;
+    serviceName: string;
+}
+
+type SchemaOutput<TSchema extends ZodRawShape> = ZodObject<TSchema>['_output'];
+
+
+export const createEnv = <TSchema extends ZodRawShape>(
+    schema: ZodObject<TSchema>,
+    options: EnvOptions,
+): SchemaOutput<TSchema> => {
+    const { source = process.env, serviceName = 'service' } = options ?? {};
+
+    const parsed = schema.safeParse(source);
+
+    if (!parsed.success) {
+        const formatedErrors = parsed.error.format();
+        const message = `Invalid environment variables for ${serviceName}: ${JSON.stringify(formatedErrors)}`;
+        throw new Error(message);
+    }
+
+    return parsed.data;
+};
+
+export type EnvSchema<TShape extends ZodRawShape> = ZodObject<TShape>;
